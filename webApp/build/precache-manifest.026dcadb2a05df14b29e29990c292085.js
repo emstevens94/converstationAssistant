cb7a2bdf8c4ffbc30898188fcf7df149abcccc9c
emstevens94 <emstevens94@gmail.com>
2019-09-16 11:58:05 -0400

    Add files via upload
    Testing build functionality for aws react app interface

---
+++ webApp/build/precache-manifest.026dcadb2a05df14b29e29990c292085.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "406dae2741ac90d175c4d09aed4dff76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7f7add2a1337991bf79",
+    "url": "/static/css/main.df9699d8.chunk.css"
+  },
+  {
+    "revision": "fc3e32f126b9c9deec95",
+    "url": "/static/js/2.f437d1b4.chunk.js"
+  },
+  {
+    "revision": "e7f7add2a1337991bf79",
+    "url": "/static/js/main.d7617bb9.chunk.js"
+  },
+  {
+    "revision": "8f22e7bf2b6e46a291a4",
+    "url": "/static/js/runtime~main.ddac8e98.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
